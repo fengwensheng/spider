953e90c2e973e6e3c771a6583fa776e8f9485dec
Vincent Feng <fengwenshenging@gmail.com>
2022-07-19 14:03:23 +0800

    create dart project

---
+++ dart_spider/bin/dart_spider.dart
@@ -0,0 +1,5 @@
+import 'package:dart_spider/dart_spider.dart' as dart_spider;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_spider.calculate()}!');
+}
---
+++ dart_spider/test/dart_spider_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_spider/dart_spider.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
